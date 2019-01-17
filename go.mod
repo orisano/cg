17f0fb266da59fa9022f36df6a812d1e8990a8db
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2019-01-17 17:11:07 +0900

    feat: improve repack command

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/orisano/cg
 
 require (
-	github.com/agnivade/levenshtein v1.0.1
-	github.com/orisano/impast v0.0.0-20190110165021-58a6894112b3
 	github.com/orisano/subflag v0.0.0-20180923144031-5e0163f95a80
 	github.com/pkg/errors v0.8.0
 	golang.org/x/tools v0.0.0-20181128225727-c5b00d9557fd
--- repack.go
+++ repack.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"go/types"
+	"sort"
 	"strings"
 
-	"github.com/agnivade/levenshtein"
 	"github.com/orisano/subflag"
 	"github.com/pkg/errors"
 	"golang.org/x/tools/go/loader"
@@ -15,12 +15,14 @@ import (
 type RepackCommand struct {
 	Source      string
 	Destination string
+	Ignores string
 }
 
 func (c *RepackCommand) FlagSet() *flag.FlagSet {
 	fs := flag.NewFlagSet("repack", flag.ExitOnError)
 	fs.StringVar(&c.Source, "src", "", "source type (required)")
 	fs.StringVar(&c.Destination, "dst", "", "destination type (required)")
+	fs.StringVar(&c.Ignores, "i", "", "ignore fields pattern (comma separated)")
 	return fs
 }
 
@@ -47,23 +49,43 @@ func (c *RepackCommand) Run(args []string) error {
 	srcType := prog.Package(srcImportPath).Pkg.Scope().Lookup(srcTypeName).Type().Underlying().(*types.Struct)
 	dstType := prog.Package(dstImportPath).Pkg.Scope().Lookup(dstTypeName).Type().Underlying().(*types.Struct)
 
-	src := walkFields(nil, srcType, "x")
-	dst := walkFields(nil, dstType, "y")
+	src := walkFields(nil, srcType, "y")
+	dst := walkFields(nil, dstType, "x")
 
-	for _, s := range src {
-		if len(dst) == 0 {
+	ignores := strings.Split(c.Ignores, ",")
+	cond := func(s string) bool {
+		fields := strings.Split(s, ".")
+		for _, field := range fields {
+			for _, x := range ignores {
+				if field == x {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
+	src = filterStrings(src, cond)
+	dst = filterStrings(dst, cond)
+
+	sort.SliceStable(dst, func(i, j int) bool {
+		return len(dst[j]) < len(dst[i])
+	})
+
+	for _, d := range dst {
+		if len(src) == 0 {
 			break
 		}
-		mini := levenshtein.ComputeDistance(s, dst[0])
-		for i := 1; i < len(dst); i++ {
-			v := levenshtein.ComputeDistance(s, dst[i])
+		mini := computeEditDistance(d, src[0])
+		for i := 1; i < len(src); i++ {
+			v := computeEditDistance(d, src[i])
 			if mini > v {
-				dst[0], dst[i] = dst[i], dst[0]
+				src[0], src[i] = src[i], src[0]
 				mini = v
 			}
 		}
-		fmt.Println(s, "=", dst[0])
-		dst = dst[1:]
+		fmt.Println(d, "=", src[0])
+		src = src[1:]
 	}
 
 	return nil
@@ -73,9 +95,19 @@ func walkFields(a []string, s *types.Struct, prefix string) []string {
 	for i := 0; i < s.NumFields(); i++ {
 		f := s.Field(i)
 		name := prefix + "." + f.Name()
-		t, ok := f.Type().Underlying().(*types.Struct)
+
+		t := f.Type()
+		ptr, ok := t.(*types.Pointer)
 		if ok {
-			a = walkFields(a, t, name)
+			t = ptr.Elem()
+		}
+		named, ok := t.(*types.Named)
+		if ok && strings.IndexByte(strings.Split(named.Obj().Pkg().Path(), "/")[0], '.') >= 0 {
+			t = t.Underlying()
+		}
+		st, ok := t.(*types.Struct)
+		if ok {
+			a = walkFields(a, st, name)
 		} else {
 			a = append(a, name)
 		}
@@ -89,4 +121,53 @@ func parseType(s string) (importPath, typeName string, err error) {
 		return "", "", errors.Errorf("invalid format %q", s)
 	}
 	return tokens[0], tokens[1], nil
+}
+
+func computeEditDistance(a, b string) int {
+	x := []rune(a)
+	y := []rune(b)
+
+	N := len(x)
+	M := len(y)
+
+	d := make([][]int, N+1)
+	for i := range d {
+		d[i] = make([]int, M+1)
+	}
+	d[0][0] = 0
+	for i := 1; i <= N; i++ {
+		d[i][0] = i
+	}
+	for i := 1; i <= M; i++ {
+		d[0][i] = i
+	}
+
+	for i := 1; i <= N; i++ {
+		for j := 1; j <= M; j++ {
+			replaceCost := 3
+			if x[i-1] == y[j-1] {
+				replaceCost = 0
+			}
+			d[i][j] = mini(d[i][j-1]+1, mini(d[i-1][j]+1, d[i-1][j-1]+replaceCost))
+		}
+	}
+	return d[N][M]
+}
+
+func mini(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func filterStrings(s []string, cond func(string)bool) []string {
+	x := s[:0]
+	for _, a := range s {
+		if cond(a) {
+			x = append(x, a)
+		}
+	}
+	return x
 }
